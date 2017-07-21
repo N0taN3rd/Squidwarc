a3d40fb1826fceb6e23e34009cd7a022c21e0bc5
Mat Kelly <me@matkelly.com>
2017-07-21 10:53:08 -0400

    Friendly defaults for macOS

--- run-chrome.sh
+++ run-chrome.sh
@@ -4,8 +4,14 @@ remoteDebugPort=9222
 chromeBinary="google-chrome-beta"
 
 
+case $OSTYPE in darwin*) 
+  chromeBinary=/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
+esac
+
+chromeVersion=$("$chromeBinary" --version | grep -oE "\d{1,4}" | head -n1)
+
 if [ "$1" = "headless" ]; then
-   $chromeBinary --headless --disable-gpu --remote-debugging-port=${remoteDebugPort}
+   "$chromeBinary" --headless --disable-gpu --remote-debugging-port=${remoteDebugPort}
 else
-   $chromeBinary --remote-debugging-port=${remoteDebugPort}
+   "$chromeBinary" --remote-debugging-port=${remoteDebugPort}
 fi
