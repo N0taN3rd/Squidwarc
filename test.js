52a768b6a4ba297fa546fda0f90adef5d35fa317
John Berlin <n0tan3rd@gmail.com>
2018-05-09 16:33:17 -0400

    Cleaned up puppeteer crawler Finalized new system for JS injection Address dev tools protocol updates in puppeteer writer

--- test.js
+++ test.js
@@ -1,15 +1,17 @@
 const puppeteer = require('puppeteer')
-const LinkCollector = require('./lib/linkCollector')
+const LinkCollector = require('./lib/injectManager')
 const DEFAULT_ARGS = require('./lib/launcher/defaultArgs')
 
-puppeteer.launch({
-  headless: false,
-  ignoreDefaultArgs: true,
-  args: DEFAULT_ARGS
-}).then(async browser => {
+async function doIt () {
+  const browser = await puppeteer.launch({
+    headless: false,
+    ignoreDefaultArgs: true,
+    args: DEFAULT_ARGS
+  })
   const page = await browser.newPage()
   await page.setViewport({width: 1920, height: 1080})
-  await page.evaluateOnNewDocument(LinkCollector.getInit())
+  const it = await page.evaluateOnNewDocument(LinkCollector.getInit())
+  console.log(it)
   page.on('console', msg => {
     if (msg.type() === 'info') {
       console.log(msg.text())
@@ -20,7 +22,9 @@ puppeteer.launch({
   console.log('doing collection')
   const results = await page.evaluate(LinkCollector.getCollect())
   console.log(results)
+  await require('fs-extra').writeJson('found.json', results)
 
   await browser.close()
-})
+}
 
+doIt().catch(err => console.error(err))
