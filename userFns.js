57fc8f488257eaa1aa89f914d8f927bd72c411f5
John Berlin <n0tan3rd@gmail.com>
2018-11-25 16:23:25 -0500

    migrated squidwarc to new version of node-warc added additional flags to launcher args to make browser usage more reliable bumped node-warc dep to 3.1.0 and now use npm installed version

--- userFns.js
+++ userFns.js
@@ -5,37 +5,50 @@
  */
 module.exports = async function (page) {
   // scrolls the page until the page cannot be scrolled
-  // some more or we have scrolled 20 times and fetches all the srcset values
+  // some more or we have scrolled 25 times and fetches all the srcset values
   let scrolled = 0
-  for (; scrolled < 20; ++scrolled) {
+  for (; scrolled < 25; ++scrolled) {
     let canScrollMore = await page.evaluate(async function () {
-      window.SEEN = window.SEEN || new Set()
+      window.$SquidwarcSeen = window.$SquidwarcSeen || new Set()
       const noop = () => {}
       const srcsetSplit = /\s*(\S*\s+[\d.]+[wx]),|(?:\s*,(?:\s+|(?=https?:)))/
-      const ss = document.querySelectorAll('*[srcset]')
+      const ss = document.querySelectorAll('*[srcset], *[data-srcset], *[data-src]')
       const fetches = []
       for (let i = 0; i < ss.length; i++) {
-        const srcset = ss[i].srcset
-        const values = srcset.split(srcsetSplit).filter(Boolean)
-        for (let j = 0; j < values.length; j++) {
-          const value = values[j].trim()
-          if (value.length > 0) {
-            const url = value.split(' ')[0]
-            if (!window.SEEN.has(url)) {
-              window.SEEN.add(url)
-              fetches.push(fetch(url).catch(noop))
+        if (ss[i].dataset.srcset || ss[i].srcset) {
+          let srcsets = []
+          if (ss[i].srcset) {
+            srcsets = srcsets.concat(ss[i].srcset.split(srcsetSplit))
+          }
+          if (ss[i].dataset.srcset) {
+            srcsets = srcsets.concat(ss[i].dataset.srcset.split(srcsetSplit))
+          }
+          for (let j = 0; j < srcsets.length; j++) {
+            if (srcsets[j]) {
+              const url = srcsets[j].trim().split(' ')[0]
+              if (!window.$SquidwarcSeen.has(url)) {
+                window.$SquidwarcSeen.add(url)
+                fetches.push(fetch(url).catch(noop))
+              }
             }
           }
         }
+        if (ss[i].dataset.src) {
+          if (!window.$SquidwarcSeen.has(ss[i].dataset.src)) {
+            window.$SquidwarcSeen.add(ss[i].dataset.src)
+            fetches.push(fetch(ss[i].dataset.src).catch(noop))
+          }
+        }
       }
-      window.scrollBy(0, 500)
       await Promise.all(fetches)
+      window.scrollBy(0, 500)
       return (
         window.scrollY + window.innerHeight <
         Math.max(document.body.scrollHeight, document.documentElement.scrollHeight)
       )
     })
+    // ensure we see all the requests by waiting for a bit before going again if we can
+    await new Promise(resolve => setTimeout(resolve, 1500))
     if (!canScrollMore) break
-    await new Promise(r => setTimeout(r, 1000))
   }
 }
